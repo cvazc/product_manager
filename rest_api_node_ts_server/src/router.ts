5f3343201a64e6666625517fb5c782cdee5ea564
Sebastian <contreras.sebastian.dev@gmail.com>
2025-01-08 19:29:27 -0600

    first commit

---
+++ rest_api_node_ts_server/src/router.ts
@@ -0,0 +1,25 @@
+import { Router } from "express"
+
+const router = Router()
+
+router.get("/", (req, res) => {
+    res.json("From GET")
+})
+
+router.post("/", (req, res) => {
+    res.json("From POST")
+})
+
+router.put("/", (req, res) => {
+    res.json("From PUT")
+})
+
+router.patch("/", (req, res) => {
+    res.json("From PATCH")
+})
+
+router.delete("/", (req, res) => {
+    res.json("From DELETE")
+})
+
+export default router
