8fc41e66c31b8bdc44126f23c5dbb2cd4201f5e4
Sebastian <contreras.sebastian.dev@gmail.com>
2025-01-13 11:49:26 -0600

    Add getProducts handler and update router to handle GET requests for products

--- rest_api_node_ts_server/src/handlers/product.ts
+++ rest_api_node_ts_server/src/handlers/product.ts
@@ -2,9 +2,17 @@ import { Request, Response } from "express"
 import { validationResult } from "express-validator"
 import Product from "../models/Product.model"
 
+export const getProducts = async (req: Request, res: Response) => {
+    try {
+        const products = await Product.findAll()
+        res.json({ data: products })
+    } catch (error) {
+        console.log(error)
+    }
+}
+
 export const createProduct = async (req: Request, res: Response) => {
     try {
-        
         const product = await Product.create(req.body)
         res.json({ data: product })
     } catch (error) {
