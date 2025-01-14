85b463e7e3bbd40b4e46b635e281ed3c9ebcad76
Sebastian <contreras.sebastian.dev@gmail.com>
2025-01-14 16:30:35 -0600

    Sort products by price in descending order and update Product model to use TypeScript's declare for properties

--- rest_api_node_ts_server/src/handlers/product.ts
+++ rest_api_node_ts_server/src/handlers/product.ts
@@ -3,7 +3,9 @@ import Product from "../models/Product.model"
 
 export const getProducts = async (req: Request, res: Response) => {
     try {
-        const products = await Product.findAll()
+        const products = await Product.findAll({
+            order: [["price", "DESC"]],
+        })
         res.json({ data: products })
     } catch (error) {
         console.log(error)
