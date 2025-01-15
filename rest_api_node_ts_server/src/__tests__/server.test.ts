d428ffbbf33d5bfd7e479684c3f400cafe301135
Sebastian <contreras.sebastian.dev@gmail.com>
2025-01-15 14:40:20 -0600

    Add Jest test script to package.json and create initial test suite

---
+++ rest_api_node_ts_server/src/__tests__/server.test.ts
@@ -0,0 +1,9 @@
+describe('First test', () => {
+    it('Check if 2 + 2 equals 4', () => {
+        expect(2 + 2).toBe(4)
+    })
+
+    it('Check if 2 + 2 not equals 5', () => {
+        expect(2 + 2).not.toBe(5)
+    })
+})
