7ef088a2661d51bf94e205013166e1af836c8473
Sebastian <contreras.sebastian.dev@gmail.com>
2025-01-16 11:32:38 -0600

    Add validation tests for product creation endpoint

--- rest_api_node_ts_server/src/handlers/__tests__/product.test.ts
+++ rest_api_node_ts_server/src/handlers/__tests__/product.test.ts
@@ -2,6 +2,45 @@ import request from "supertest"
 import server from "../../server"
 
 describe("POST /api/products", () => {
+    it("Should display validation errors", async () => {
+        const response = await request(server).post('/api/products').send({})
+
+        expect(response.status).toBe(400)
+        expect(response.body).toHaveProperty('errors')
+        expect(response.body.errors).toHaveLength(4)
+
+        expect(response.status).not.toBe(404)
+        expect(response.body.errors).not.toHaveLength(2)
+    })
+
+    it("Should validate product price is greater than 0", async () => {
+        const response = await request(server).post('/api/products').send({
+            name: "Mouse - Testing",
+            price: 0,
+        })
+
+        expect(response.status).toBe(400)
+        expect(response.body).toHaveProperty('errors')
+        expect(response.body.errors).toHaveLength(1)
+
+        expect(response.status).not.toBe(404)
+        expect(response.body.errors).not.toHaveLength(2)
+    })
+
+    it("Should validate that the price is a number and greater than 0", async () => {
+        const response = await request(server).post('/api/products').send({
+            name: "Mouse - Testing",
+            price: "Hello",
+        })
+
+        expect(response.status).toBe(400)
+        expect(response.body).toHaveProperty('errors')
+        expect(response.body.errors).toHaveLength(2)
+
+        expect(response.status).not.toBe(404)
+        expect(response.body.errors).not.toHaveLength(4)
+    })
+
     it("Should create a new product", async () => {
         const response = await request(server).post("/api/products").send({
             name: "Mouse - Testing",
@@ -13,6 +52,6 @@ describe("POST /api/products", () => {
 
         expect(response.status).not.toBe(404)
         expect(response.status).not.toBe(200)
-        expect(response.body).not.toHaveProperty('error')
+        expect(response.body).not.toHaveProperty('errors')
     })
 })
