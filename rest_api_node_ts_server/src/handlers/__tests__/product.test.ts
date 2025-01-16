8d8128f345eda1afd119d5d1f09a29fdb69aea53
Sebastian <contreras.sebastian.dev@gmail.com>
2025-01-16 12:00:39 -0600

    Refactor product validation tests and add GET /api/products tests

--- rest_api_node_ts_server/src/handlers/__tests__/product.test.ts
+++ rest_api_node_ts_server/src/handlers/__tests__/product.test.ts
@@ -3,10 +3,10 @@ import server from "../../server"
 
 describe("POST /api/products", () => {
     it("Should display validation errors", async () => {
-        const response = await request(server).post('/api/products').send({})
+        const response = await request(server).post("/api/products").send({})
 
         expect(response.status).toBe(400)
-        expect(response.body).toHaveProperty('errors')
+        expect(response.body).toHaveProperty("errors")
         expect(response.body.errors).toHaveLength(4)
 
         expect(response.status).not.toBe(404)
@@ -14,13 +14,13 @@ describe("POST /api/products", () => {
     })
 
     it("Should validate product price is greater than 0", async () => {
-        const response = await request(server).post('/api/products').send({
+        const response = await request(server).post("/api/products").send({
             name: "Mouse - Testing",
             price: 0,
         })
 
         expect(response.status).toBe(400)
-        expect(response.body).toHaveProperty('errors')
+        expect(response.body).toHaveProperty("errors")
         expect(response.body.errors).toHaveLength(1)
 
         expect(response.status).not.toBe(404)
@@ -28,13 +28,13 @@ describe("POST /api/products", () => {
     })
 
     it("Should validate that the price is a number and greater than 0", async () => {
-        const response = await request(server).post('/api/products').send({
+        const response = await request(server).post("/api/products").send({
             name: "Mouse - Testing",
             price: "Hello",
         })
 
         expect(response.status).toBe(400)
-        expect(response.body).toHaveProperty('errors')
+        expect(response.body).toHaveProperty("errors")
         expect(response.body.errors).toHaveLength(2)
 
         expect(response.status).not.toBe(404)
@@ -48,10 +48,27 @@ describe("POST /api/products", () => {
         })
 
         expect(response.status).toBe(201)
-        expect(response.body).toHaveProperty('data')
+        expect(response.body).toHaveProperty("data")
 
         expect(response.status).not.toBe(404)
         expect(response.status).not.toBe(200)
-        expect(response.body).not.toHaveProperty('errors')
+        expect(response.body).not.toHaveProperty("errors")
+    })
+})
+
+describe("GET /api/products", () => {
+    it("Should check if api/products url exists", async () => {
+        const response = await request(server).get("/api/products")
+        expect(response.status).not.toBe(404)
+    })
+
+    it("GET a JSON response with products", async () => {
+        const response = await request(server).get("/api/products")
+
+        expect(response.status).toBe(200)
+        expect(response.headers["content-type"]).toMatch(/json/)
+        expect(response.body).toHaveProperty("data")
+        expect(response.body.data).toHaveLength(1)
+        expect(response.body).not.toHaveProperty("errors")
     })
 })
